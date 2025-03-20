57354b8e53fec7cdec455e420329f227951e28ec
Yuvan Sharma <yuvan.sharma@gmail.com>
2025-03-20 13:55:10 -0700

    removed devel, build from github

--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/cc/ee106b/sp25/class/ee106b-aca/ros_workspace/project3/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/turtlebot_proj3_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/ee106b/sp25/class/ee106b-aca/ros_workspace/project3/src/turtlebot_proj3_pkg
-# Build directory: /home/cc/ee106b/sp25/class/ee106b-aca/ros_workspace/project3/build/turtlebot_proj3_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
